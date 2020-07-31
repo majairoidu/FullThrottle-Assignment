5ca4cef23aedbec12bff2f30a457cb64780fa35b
Majai <majairoidu@gmail.com>
2020-07-31 09:02:22 +0530

    Updates

---
+++ precache-manifest.4b3c56602ac84ecaa41fed188ac25d7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db3128cb263f85eb365661dffb0c3f3",
+    "url": "/FullThrottle_Labs/index.html"
+  },
+  {
+    "revision": "854ee6046d54d3731a8a",
+    "url": "/FullThrottle_Labs/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "8cbceaeec110901f4117",
+    "url": "/FullThrottle_Labs/static/css/main.854dee9f.chunk.css"
+  },
+  {
+    "revision": "854ee6046d54d3731a8a",
+    "url": "/FullThrottle_Labs/static/js/2.275e2aa7.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/FullThrottle_Labs/static/js/2.275e2aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cbceaeec110901f4117",
+    "url": "/FullThrottle_Labs/static/js/main.6fe3034b.chunk.js"
+  },
+  {
+    "revision": "08c07a770489eb19b436",
+    "url": "/FullThrottle_Labs/static/js/runtime-main.c6aff96e.js"
+  }
+]);
